59441c9e15c7b75734d10b9adc37ab4060d3334b
cloudgirlsin <64674076+cloudgirlsin@users.noreply.github.com>
2021-11-01 22:22:33 -0300

    Ajuste Pagina Contato
    Ajuste arquivo php

--- contato/contactform.php
+++ contato/contactform.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit'])) {
      $message = $_POST['message'];
      $subject= $_POST['subject'];
 
-     $mailTo="contato@crechetiasol.com.br";
+     $mailTo="danilo.com@gmail.com";
      $headers="From: ".$mailFrom;
      $subject="".$subject;
      $txt="Nome:" .$name. ".\nTel:" .$phone. ".\n\n".$message;
